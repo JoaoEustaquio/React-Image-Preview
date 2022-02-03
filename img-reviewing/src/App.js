dfb106972646a7801a3fe018032fa83d1e57d1d2
Joao_Godinho <git.qodeless@outlook.com>
2022-02-03 17:38:13 -0300

    Basic Structure

--- img-reviewing/src/App.js
+++ img-reviewing/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state={
+    profileimg:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
+  }
+  render() {
+    const {profileimg} = this.state
+
+    return (
+      <div className='page'>
+        <div className='container'>
+          <h1 className='heading'>Add your Image</h1>
+          <div className='img-holder'>
+            <img src={profileimg} alt="" id="img" className='img'></img>
+          </div>
+          <input type="file" name="image-upload" id="input" accept='image/*'/>
+          <div className='label'>
+            <label htmlFor="input" className='image-upload'>
+              <i className='material-icons'>add_photo_alternate</i>
+              Choose your photo
+            </label>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
